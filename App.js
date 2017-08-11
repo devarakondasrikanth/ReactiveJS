4a82b33e90053e8601e52b0942fa23b4b01ac82c
Srikanth <devarakondasrikanth@users.noreply.github.com>
2017-08-11 15:18:55 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            Hai Srikanth Welcome to the World of ReactJS!!!
+        </div>
+    );
+    }
+}
+
+export default App;
