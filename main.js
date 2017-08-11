1f90180ed341f3d50888c874dd7a9e3c4c422e32
Srikanth <devarakondasrikanth@users.noreply.github.com>
2017-08-11 15:19:39 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import Variable from './Variables.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Variable />, document.getElementById('variable'));
